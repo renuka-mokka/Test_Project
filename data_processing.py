d4b829dea084df94edd214fa5ad68600a7181a5e
renuka-mokka <renuka.mokka@gmail.com>
2023-08-03 18:36:52 +0530

    addition and multiplication functions added in code

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,14 @@
 
 print("this is data processing pyhton file")
+
+a=5
+b=6
+def sum(a,b):
+    c=a+b
+    return c
+print("sum function added to get correct results", sum(2, 3))
+
+def mul(a,b):    
+    c=a*b
+    return c
+print("multiplication result:", mul(5, 8))
